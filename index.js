8cde71fbfd49816dfee38d9a29606044f8597470
Michael Jeung <michaelj229@gmail.com>
2023-03-26 14:16:12 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, ele){
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === ele.toLowerCase()
+    })
+};
+
+function fuzzyMatch(drivers, ele){
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, ele.length) === ele.toLowerCase()
+    })
+}
+
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) {
+        return driver.name === argument
+    })
+}
